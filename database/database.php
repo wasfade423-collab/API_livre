dc30ec26be9a264e800e40809510bd7d3ddb7515
wasfade <wasfade423@gmail.com>
2026-02-24 14:16:42 +0100

    API Livre

--- database/database.php
+++ database/database.php
@@ -1,4 +1,7 @@
 <?php
+    try{
+
+    }cach
     $database = new PDO("mysql:host=localhost; dbname=apiLivre; charset=utf8", 'root', '');
     $database->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
